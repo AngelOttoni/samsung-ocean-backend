31d44f66a2cc3fa17ef1c83ab3192c0326e5f871
AngelOttoni <angel.m.ottoni@gmail.com>
2021-10-06 11:31:59 -0300

    README e endpoint Read All adicionados

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ app.get("/oi", function (req, res) {
     res.send("Olá, Mundo!");
 });
 
+// Lista de heróis
+
+const lista = ["Wonder Woman", "Catwoman", "Supergirl"];
+
+// Endpoint de Read All
+
+app.get("/herois", function (req, res) {
+    res.send(lista);
+});
+
 app.listen(3000);
